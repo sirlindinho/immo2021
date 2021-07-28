3c3f8a3aa062ce9a355e499ea773cfaa42eea602
sirlindinho <56820309+sirlindinho@users.noreply.github.com>
2021-07-28 15:52:57 +0200

    DB

---
+++ js/app.js
@@ -0,0 +1,34 @@
+class app {
+
+    constructor() {
+        let firebaseConfig = {
+            apiKey: "AIzaSyANzNCm5l4rrBzV5Ki0iJEZNjz-6ga4SI4",
+            authDomain: "immo-webprog-2019.firebaseapp.com",
+            databaseURL: "https://immo-webprog-2019.firebaseio.com",
+            projectId: "immo-webprog-2019",
+            storageBucket: "immo-webprog-2019.appspot.com",
+            messagingSenderId: "249226224804",
+            appId: "1:249226224804:web:5861c61756df569252f231",
+            measurementId: "G-FJDCMVG1JY"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        firebase.analytics();
+
+    function Writevariabledata() {
+        var name = document.getElementById("name");
+        var email = document.getElementById("email");
+        var phone = document.getElementById("phone");
+        var message = document.getElementById("message");
+        var docRef = db.collection("Kontaktnachrichten").add({
+            name,
+            email,
+            phone,
+            message,
+        });
+
+        docRef.push().set(messagetext);
+        window.alert("Inserted Successfully..");
+    }
+}}
+
